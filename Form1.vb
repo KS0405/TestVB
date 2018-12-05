845e0748a7231cb7347ddc63d8aa2645e97077a8
KS0405 <45250709+KS0405@users.noreply.github.com>
2018-12-05 21:15:55 +0700

    LabTest

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        lblYear.Text = txtSalary.Text * 12
+        lblVat.Text = txtSalary.Text * 5 / 100
+        lblNet.Text = lblYear.Text - lblVat.Text
+    End Sub
+End Class
